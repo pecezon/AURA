3811a1adc1854e459b79b92eee5cea932d9beb81
pecezon <lopez.diego@cetys.edu.mx>
2026-03-01 23:22:51 -0800

    Minor Changes

--- frontend/src/components/cajita-test.tsx
+++
@@ -1,7 +0,0 @@
-export default function CajitaTest() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Cajita Test</h1>
-    </div>
-  );
-}
