ca366bf4fdb06ecd25508eb2fb11c122bd398ad8
pecezon <lopez.diego@cetys.edu.mx>
2026-03-04 12:59:25 -0800

    Query Provider Setup In Main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppRouter } from "./router";
 import "./index.css";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppRouter />
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
