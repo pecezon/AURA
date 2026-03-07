7c273cce3b24983095449a203020087f8db06e7c
pecezon <lopez.diego@cetys.edu.mx>
2026-03-07 14:43:54 -0800

    feat: Add DropdownMenu component with various menu items and functionalities

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -2,10 +2,12 @@ import { useEffect } from "react";
 import { useNavigate } from "@tanstack/react-router";
 import { api } from "../lib/api";
 import LogoutButton from "../components/logout-button";
+import { Navbar } from "../components/navbar";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="flex flex-col items-center min-h-screen bg-gray-100">
+      <Navbar />
       Bienvenido al Dashboard
       <LogoutButton />
     </div>
