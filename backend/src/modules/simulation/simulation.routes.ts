1909acd8d32dd9b264301c655005cda46322bab2
pecezon <lopez.diego@cetys.edu.mx>
2026-05-16 17:56:59 -0700

    feat(backend): exponer endpoints para consulta y envio de simulaciones
    - Modificacion de SimulationService para exponer configuraciones JSON y hotspots.
    
    - Actualizacion de simuAttempt.service para orquestar la llamada a ScoringService y guardar resultados.

--- backend/src/modules/simulation/simulation.routes.ts
+++ backend/src/modules/simulation/simulation.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createNewSimulation, getSimulationByModuleId, getSimulationByTitle, updateSimulation } from "./simulation.controller";
+import { createNewSimulation, getSimulationById, getSimulationByModuleId, getSimulationByTitle, updateSimulation } from "./simulation.controller";
 
 
 const router = Router()
 
 router.get("/simulation-by-module/:moduleId", getSimulationByModuleId)
+router.get("/:simulationId", getSimulationById)
 router.get("/simulation-by-title", getSimulationByTitle)
 router.post("/simulation-create", createNewSimulation)
 router.put("/update-simulation/:simulationId", updateSimulation)
